502015eae86ee0bf04827a018085729d02c3445a
Yazid MERZAK <yazid.merzak@novanexx.com>
2022-06-01 20:46:52 +0200

    feat : navbar creation

--- Watsup/tailwind.config.js
+++ Watsup/tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  theme: {
+    colors:{
+      'watsup-bg-main' : '#212332',
+      'watsup-bg-secondary' : '#2A2D3E',
+      'watsup-title' : '#9B8EFF',
+      'watsup-text' : '#D8D7E1',
+      'watsup-text-white' : '#FFFFFF'
+    },
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'] 
+      }
+    }
+  }
 }
