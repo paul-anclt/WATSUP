c70da7ed7aa2f7fbb36f5075a39b955a9c698daf
Yazid MERZAK <yazid.merzak@novanexx.com>
2022-06-02 20:22:49 +0200

    feat : update dashboard

--- Watsup/src/app/app-routing.module.ts
+++ Watsup/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent},
 ];
 
 @NgModule({
--- Watsup/src/app/dashboard/dashboard.component.ts
+++ Watsup/src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  host: {'class': 'w-4/6'}
+
 })
 export class DashboardComponent implements OnInit {
 
