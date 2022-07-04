7f4f3cda73f2fc3ab39d6ada2e9f446c0700ba09
Paul Ancelet <paul21ancelet@orange.fr>
2022-07-04 13:17:03 +0200

    feat : Add login & register

--- Watsup/src/app/app-routing.module.ts
+++ Watsup/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { TradingComponent } from './trading/trading.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'trading', component: TradingComponent}
+  { path: 'trading', component: TradingComponent},
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
