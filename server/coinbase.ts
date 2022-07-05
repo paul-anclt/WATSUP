036e5d9cc401d6e1c3181fa68ee1f70a7979e7c0
Vinmania <vinmaniamc@gmail.com>
2022-07-05 23:14:51 +0200

    fix creditentials

--- server/coinbase.ts
+++ server/coinbase.ts
@@ -4,8 +4,8 @@
 // var request = require('request');
 
 // // Set these in your ENVironment, or enter them here with the actual string
-// var apiKey = 'kPy0MYfr6oTldlMs';
-// var apiSecret = 'uaxgYTtWGkwW7IqmagTYq6DpCb1s1wGL';
+// var apiKey = '';
+// var apiSecret = '';
 
 
 // //get unix time in seconds
