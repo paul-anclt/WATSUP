08b910517cec3e834d34a28e38bbdf90aa72d5e6
Sami-EB <dutinfo123@gmail.com>
2022-07-06 17:29:02 +0200

    create ts binance class

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Spot } = require('@binance/connector')
+const { BinanceAPI } = require('./binance.ts');
 
 const app = express();
 
@@ -11,31 +11,4 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-
-// BINANCE /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-const binanceApiKey = '3tVDks7TYCzNNuhn1s6r1jWpaU4VGZYdeGYasFTsydRwkWhczfg0coPoVZiyp89q'
-const binanceApiSecret = 'tvBQ98lY8VxgsgnHTyHp5OEWjgrr3IrJvWCnHyOWN1ZRaYh8DOva21GqdgK29bLh'
-const binanceClient = new Spot(binanceApiKey, binanceApiSecret, { baseURL: 'https://testnet.binance.vision'}) // base url for test accounts/keys
-
-// Get account information
-app.get('/binanceInfo', (req, res) => {
-    binanceClient.account().then(response => {
-        binanceClient.logger.log(response.data)
-        res.send(response.data)
-    })
-})
-
-// Place a new order
-app.get('/binanceOrder', (req, res) => {
-    binanceClient.newOrder('BNBUSDT', 'BUY', 'LIMIT', {
-        price: '350',
-        quantity: 1,
-        timeInForce: 'GTC'
-    }).then(response => {
-        binanceClient.logger.log(response.data)
-        res.send(response.data)
-    }).catch(error => binanceClient.logger.error(error))
-})
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const binanceApiClient = new BinanceAPI();
